7f8c69b8bd831044a6346d34922e5b3f083411b2
Zenaking14 <pierceet@oregonstate.edu>
2021-12-13 19:14:38 -0800

    fixed an error with api retrieval

--- commands/utils/abilities.js
+++ commands/utils/abilities.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const base_url = 'https://pokeapi.co/api/ability'
+const base_url = 'https://pokeapi.co/api/v2/ability'
 
 async function getAbility(ability) {
     let response = await fetch(`${base_url}/${ability}`)
